4fb05dc7b4e9c9b0f6cd100e4622df132e7f5529
Dhwani236 <dhwani2336@gmail.com>
2025-11-01 13:26:13 +0530

    Add files via upload

---
+++ models/1.py
@@ -0,0 +1,6 @@
+''' 
+no models yet because we aren't classifying the defects. this solution only aims at a yes/no approach: confirms the presence of defects on the 
+surface of the given wafer 
+
+'''
+
